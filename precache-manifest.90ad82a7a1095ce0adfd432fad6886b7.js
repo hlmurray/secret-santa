998e3e298b5be73b8f78efa173edbed95f587851
hlmurray91 <hlmurray91@gmail.com>
2020-10-16 15:04:45 -0400

    Updates

---
+++ precache-manifest.90ad82a7a1095ce0adfd432fad6886b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db0d71126b8d855740feb003f1c53b2",
+    "url": "/secret-santa/index.html"
+  },
+  {
+    "revision": "94903e838c5a5b8c3019",
+    "url": "/secret-santa/static/css/main.2b3f72c4.chunk.css"
+  },
+  {
+    "revision": "96db6f00da40d502fca0",
+    "url": "/secret-santa/static/js/2.f798c059.chunk.js"
+  },
+  {
+    "revision": "f110a4fa7ae7043edab997fe52455303",
+    "url": "/secret-santa/static/js/2.f798c059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94903e838c5a5b8c3019",
+    "url": "/secret-santa/static/js/main.6e4014a0.chunk.js"
+  },
+  {
+    "revision": "ed2f97ad8324ae2cbc48",
+    "url": "/secret-santa/static/js/runtime-main.1b070df3.js"
+  }
+]);
